50b54e60c91d2ecb3df6c0095c4f01522e2d4798
Pedro <p-spielmann@hotmail.com>
2018-05-14 00:31:49 -0300

    no message

--- Atletismo.sql
+++ Atletismo.sql
@@ -36,6 +36,40 @@ foreign key(id_prova) references prova(id_prova),
 foreign key(id_atleta) references atleta(codigo)
 )
 
+insert into prova (nome_prova, sexo) values
+('Lançamento do Dardo', 'f'),
+('Salto em Distancia', 'm'),
+('Salto com Vara', 'm'),
+('400m com barreira', 'm'),
+('100m','f'),
+('Arremesso de Peso','f'),
+('100m','m'),
+('3000m','m'),
+('Lançamento de Disco','m'),
+('3000m com obstaculo','f'),
+('Salto Triplo','f'),
+('400m','m'),
+('800m','f'),
+('800m','m'),
+('200m','f'),
+('200m','m')
+
+create function fn_retorna_prova ()
+returns @prova table(
+
+id_prova int ,
+nome_prova varchar(50),
+sexo char(1)
+)
+as
+begin
+	insert into @prova 
+	select * from prova
+return
+end
+
+select * from fn_retorna_prova()
+
 create function fn_bateria (@id_prova int, @bateria int)
 returns @resultado table(
 id_prova int,
